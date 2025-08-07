8c3b23f8ff24306382dd9b293f90016dca002941
Durjoy <swe.eng.durjoy15@gmail.com>
2025-08-07 13:15:40 +0600

    first commit

---
+++ Introduction/main.py
@@ -0,0 +1,9 @@
+print("Hello, Durjoy!")
+print("Welcome to the Introduction module.", end=" ")
+print("This is a simple Python script.")
+a = 5
+b = 10
+c = a + b
+print("The sum of", a, "and", b, "is", c)
+
+# ctrl + / for commenting
